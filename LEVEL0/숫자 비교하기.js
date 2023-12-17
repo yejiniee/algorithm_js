d251389e35389daec081c6c1342598b7be4ee4e5
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2023-12-17 18:48:57 +0900

    Create 숫자 비교하기.js

---
+++ LEVEL0/숫자 비교하기.js
@@ -0,0 +1,13 @@
+function solution(num1, num2) {
+    var answer = 0;
+    if(num1===num2)
+        return 1;
+    else
+        return -1;
+}
+
+//다른 사람의 풀이
+function solution(num1, num2) {
+    var answer = num1 === num2 ? 1 : -1;
+    return answer;
+}
