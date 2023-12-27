e8a7241175bc9f364e8d6256b2f2041b92625f78
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2023-12-28 00:39:43 +0900

    Create 옷가게 할인 받기.js

---
+++ LEVEL0/옷가게 할인 받기.js
@@ -0,0 +1,11 @@
+function solution(price) {
+    var answer = 0;
+    if(price>=500000){
+        answer=Math.floor(price*0.8);
+    }else if(price>=300000){
+        answer=Math.floor(price*0.9);
+    }else if(price>=100000){
+        answer=Math.floor(price*0.95);
+    }
+    return answer;
+}
